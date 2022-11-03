7335f3432f79c460b26597c63d4bf2aa17fc0cf1
fullstackAnkit <jhaankit222@gmail.com>
2022-11-03 13:14:35 +0530

    code fixed for prime

--- primenum.js
+++ primenum.js
@@ -1,10 +1,20 @@
-function checkPrimeNum(num){
-     for(let i=1;i<=num;i++){
-       if(num==i){
-        return true;
+function checkPrime(number){
+    let factors=0;
+    for(let i=1;i<=num;i++){
+       if(num%i==0){
+        factors++;
        }
+     }
+     if(factors==2){
+          return true;
      }
      return false;                         
 }
 
-checkPrimeNum(17);
+let answer=checkPrime(17);
+
+if(answer==true){
+   console.log("Prime Number");
+}else{
+   console.log("Not a Prime Number");
+}
